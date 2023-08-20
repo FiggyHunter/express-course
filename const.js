2995a1052bc96c14a37bb4f6c626fd7bd555c7be
FiggyHunter <leonardo.roi3@gmail.com>
2023-08-20 16:56:51 +0200

    include global const file

---
+++ const.js
@@ -0,0 +1,12 @@
+export const SALT_ROUNDS = 10;
+export const SECRET = "DGT44";
+
+export const ROLES = {
+  USER: "user",
+  ADMIN: "admin",
+};
+
+export const PERMISSIONS = {
+  user: [],
+  admin: ["GET/"],
+};
